1329b4f4aed715fdea5eae0824b0f0799c51e17f
hashrock <hashrock@users.noreply.github.com>
2017-11-11 02:26:09 +0900

    init

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="main">
+    <h1>Hello World!</h1>
+    <input v-model="text">
+    <p>input: {{text}}</p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class App extends Vue {
+  text: string = "Hello There";
+  mounted() {}
+}
+</script>
+
